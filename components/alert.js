2d690a689fe8cc6e06d7c88fa6c5a87aa84720c1
Marvin Oey <marvin.oey@contentful.com>
2022-04-15 14:45:52 -0400

    Add github repo environment variable

--- components/alert.js
+++ components/alert.js
@@ -1,8 +1,8 @@
-import Container from './container'
-import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
+import Container from './container';
+import cn from 'classnames';
+import {EXAMPLE_PATH} from '../lib/constants';
 
-export default function Alert({ preview }) {
+export default function Alert({preview}) {
   return (
     <div
       className={cn('border-b', {
@@ -27,7 +27,7 @@ export default function Alert({ preview }) {
             <>
               The source code for this blog is{' '}
               <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+                href={process.env.GITHUB_REPO_PATH}
                 className="underline hover:text-success duration-200 transition-colors"
               >
                 available on GitHub
@@ -38,5 +38,5 @@ export default function Alert({ preview }) {
         </div>
       </Container>
     </div>
-  )
+  );
 }
